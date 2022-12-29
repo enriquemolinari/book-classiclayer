cba524c13846fc837388a19774d6a04edc16d3d3
Enrique <emolinari@jusrionegro.gov.ar>
2022-12-29 13:05:11 -0300

    adding inserts

--- src/main/java/ar/main/SetUpDatabase.java
+++ src/main/java/ar/main/SetUpDatabase.java
@@ -1,6 +1,8 @@
 package ar.main;
 
 import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.result.ResultBearing;
+import org.jdbi.v3.core.statement.Update;
 
 public class SetUpDatabase {
 
@@ -43,6 +45,7 @@ public class SetUpDatabase {
           + "primary key generated always as identity (start with 1,increment by 1), "
           + "username VARCHAR(255), password VARCHAR(255), "
           + "id_person INT not null, "
+          + "points INT not null, "          
           + "foreign key (id_person) references person)");
 
       handle.execute("CREATE TABLE users_audit (id_user_audit INT NOT NULL "
@@ -69,9 +72,52 @@ public class SetUpDatabase {
           + "id_movie INT not null, value decimal not null, "
           + "foreign key (id_movie) references movie)");
 
-      // handle.createUpdate("INSERT INTO movie (name) VALUES (?)")
-      // .bind(0, "Esperando la carroza").execute();
+      Update updateSchool = handle.createUpdate("INSERT INTO movie (name, duration) VALUES (?, ?)")
+       .bind(0, "School of Rock")
+       .bind(1, "109");
 
+      ResultBearing schoolMovie = updateSchool.executeAndReturnGeneratedKeys();
+      
+      Update updateBigFish = handle.createUpdate("INSERT INTO movie (name, duration) VALUES (?, ?)")
+      .bind(0, "Big Fish")
+      .bind(1, "125");
+      
+      ResultBearing bigFishMovie = updateBigFish.executeAndReturnGeneratedKeys();
+   
+      Update updateJack = handle.createUpdate("INSERT INTO person (name, surname, email) VALUES (?, ?, ?)")
+      .bind(0, "Jack")
+      .bind(1, "Black")
+      .bind(2, "jack.black@mymovies.com");      
+
+      ResultBearing jackBlack = updateJack.executeAndReturnGeneratedKeys();
+
+      
+      handle.createUpdate("INSERT INTO person (name, surname, email) VALUES (?, ?, ?)")
+      .bind(0, "Mike")
+      .bind(1, "White")
+      .bind(2, "mike.white@mymovies.com")      
+      .execute();
+
+      handle.createUpdate("INSERT INTO person (name, surname, email) VALUES (?, ?, ?)")
+      .bind(0, "Ewan")
+      .bind(1, "McGregor")
+      .bind(2, "ewan.mcgregor@mymovies.com")      
+      .execute();
+
+      handle.createUpdate("INSERT INTO person (name, surname, email) VALUES (?, ?, ?)")
+      .bind(0, "Albert")
+      .bind(1, "Finney")
+      .bind(2, "albert.finney@mymovies.com")      
+      .execute();
+      
+      handle.createUpdate("INSERT INTO person (name, surname, email) VALUES (?, ?, ?)")
+      .bind(0, "Enrique")
+      .bind(1, "Molinari")
+      .bind(2, "enrique.molinari@gmail.com")      
+      .execute();
+      
+      
+      
     });
 
     // List<User> users = jdbi.withHandle(handle -> {
